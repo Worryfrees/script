d5fce815e39ed03c5c5279349dbf75be2a18632f
Worryfree <xxbb@live.com>
2022-06-25 17:37:25 +0800

    Update CamScanner.js

--- Rewrites/other/CamScanner.js
+++ Rewrites/other/CamScanner.js
@@ -2,8 +2,10 @@
 CamScanner unlocks pro, Cloud scanning is not available.
 QuanX 1.0.0:  [rewrite_local]
 ^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
-Quan:  [REWRITE]
-https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url simple-response SFRUUC8xLjEgMjAwIE9LCgp7CiAiZGF0YSI6IHsKICAicHNubF92aXBfcHJvcGVydHkiOiB7CiAgICJleHBpcnkiOiAiMTY0MzczMTIwMCIKICB9CiB9Cn0=
+
+Loon:[Script]
+http-response https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? script-path=https://raw.githubusercontent.com/Worryfrees/script/main/Rewrites/other/CamScanner.js, requires-body=true, timeout=10, tag=CamScanner unlocks pro
+
 Surge4.0: [Script]
 http-response https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
 QX & Q & Surge Mitm = ap*.intsig.net,
